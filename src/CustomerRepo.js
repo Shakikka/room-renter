9207517184fdb09dd54eeccc8953b438f80d153d
Joel <wilddroppings@gmail.com>
2021-03-06 20:46:12 -0700

    Create CustomerRepo test and Class

---
+++ src/CustomerRepo.js
@@ -0,0 +1,12 @@
+import {Customer} from './Customer';
+export class CustomerRepo {
+  constructor(customersData) {
+    this.customers = []
+    this.createCustomers(customersData)
+
+  }
+
+  createCustomers(customersData) {
+    this.customers = customersData.map(customer => new Customer(customer))
+  }
+}
--- test/Customer-test.js
+++ test/Customer-test.js
@@ -1,19 +1,17 @@
 import {expect} from 'chai';
 import {Customer} from '../src/Customer';
+import {CustomerRepo} from '../src/customerRepo';
 
 describe('Customer', function() {
-  let users, user1, user2
+  let users, customerRepo
+
   beforeEach(function() {
     users = [{id: 17, name: 'La Luna'}, {id: 43, name: 'El Sol'}];
-    user1 = new Customer(users[0]);
-    user2 = new Customer(users[1]);
+    customerRepo = new CustomerRepo(users)
   });
 
-  it('should have an id', function() {
-    expect(user2.id).to.equal(43)
+  it('should accept an array of customer data and create customer classes', function() {
+    expect(customerRepo.customers).to.deep.equal(users)
   });
 
-  it('should have a name', function() {
-    expect(user1).to.deep.equal(users[0])
-  });
 });
